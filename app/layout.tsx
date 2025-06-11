951315cadc5a9523e03594774d89f3287cd4d67b
white-duke <identifiantapple.ipad6@gmail.com>
2025-06-11 10:21:31 +0200

    aze

--- app/layout.tsx
+++ app/layout.tsx
@@ -70,7 +70,7 @@ export default async function RootLayout({
       >
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="white"
           enableSystem
           disableTransitionOnChange
         >
